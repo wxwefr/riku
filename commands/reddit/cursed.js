d660902c899007e8f2d10d603664a8f5d187ef96
Variety_Sensei <53715423+VarietySensei@users.noreply.github.com>
2020-02-12 23:08:13 +0800

    Update cursed.js

--- commands/reddit/cursed.js
+++ commands/reddit/cursed.js
@@ -6,11 +6,11 @@ module.exports = {
     category: "reddit",
     description: "Sends an cursed image",
     run: async (client, message, args) => {
-<<<<<<< HEAD
+
         const subReddits = ["Cursed_Images", "cursedimages", "MakeMeSuffer","cursed_cats", "cursedvideos", "cursedcomments", "cursed_or_blessed", "cursedmemes", "CursedMinecraft"];
 =======
         const subReddits = ["Cursed_Images", "cursedimages", "MakeMeSuffer","cursed_cats", "cursedvideos", "cursedcomments", "cursed_or_blessed", "cursedmemes"];
->>>>>>> 8eb7e6f23c3e2fb89b7e1b210e89d3ee959c86e2
+
         const random = subReddits[Math.floor(Math.random() * subReddits.length)];
 
         const img = await randomPuppy(random);
